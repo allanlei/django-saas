c4558626b01079a73ad63068a9b9e339b0158e2f
Allan Lei <allanlei@helveticode.com>
2011-04-27 11:14:08 -0700

    Implement createdb (only psql2)

--- saas/multidb/management/commands/createdb.py
+++ saas/multidb/management/commands/createdb.py
@@ -4,73 +4,56 @@ from django.conf import settings
 from django.db import connection
 
 from optparse import make_option
-
+import subprocess
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
-        make_option('--username', action='store', dest='username', default='', help='Database username'),
-        make_option('--password', action='store', dest='password', default='', help='Database password'),
-        make_option('--host', action='store', dest='host', default='', help='Database host'),
-        make_option('--port', action='store', dest='port', default='', help='Database port'),
+        make_option('--engine', action='store', dest='engine', default=settings.DATABASES['default']['ENGINE'], help='Database engine'),
+        make_option('--username', action='store', dest='username', default=settings.DATABASES['default']['USER'], help='Database username'),
+        make_option('--host', action='store', dest='host', default=settings.DATABASES['default']['HOST'], help='Database host'),
+        make_option('--port', action='store', dest='port', default=settings.DATABASES['default']['PORT'], help='Database port'),
     )
-    help = 'Creates a new empty database'
+    help = 'Creates a new empty database to be used with syncdb. SQLite3 does not need to be created as syncdb will create it'
     
-    def handle(self, name, engine, **options):
+    def handle(self, name, **options):
+        engine = options['engine']
+        username = options['username']
+        hostname = options['host']
+        port = options['port']
         if engine == 'django.db.backends.postgresql_psycopg2':
-            self.create_postgresql_psycopg2(name, **options)
+            #hostname:port:database:username:password in ~/.pgpass
+            return self.create_postgresql_psycopg2(name, username=username, hostname=hostname, port=port)
+        elif engine == 'django.db.backends.sqlite3':
+            return self.create_sqlite3(name)
+        elif engine == 'django.db.backends.postgresql':
+            return self.create_postgresql(name)
+        elif engine == 'django.db.backends.mysql':
+            return self.create_mysql(name)
+        elif engine == 'django.db.backends.oracle':
+            return self.create_oracle(name)
     
-    def create_postgresql_psycopg2(self, name, **options):
-        isolation_level = connection.isolation_level
-        connection.isolation_level = 0
-        cursor = connection.cursor()
-        cursor.execute('CREATE DATABASE %s', [name])
-        connection.isolation_level = isolation_level
+    def run_command(self, cmd):
+        try:
+            return subprocess.check_call(cmd)
+        except subprocess.CalledProcessError, ex:
+            return ex.returncode
+            
+    def create_postgresql_psycopg2(self, name, username=None, hostname=None, port=5432):
+        cmd = ['createdb']
+        if username: cmd.append('--username=%s' % username)
+        if hostname: cmd.append('--hostname=%s' % hostname)
+        if port: cmd.append('--port=%s' % port)
+        cmd.append(name)
+        return self.run_command(cmd)
     
+    def create_sqlite3(self, name):
+        pass
     
-    from django.db import connection, transaction
-    cursor = connection.cursor()
-
-    # Data modifying operation - commit required
-    cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [self.baz])
-    transaction.commit_unless_managed()
-
-    # Data retrieval operation - no commit required
-    cursor.execute("SELECT foo FROM bar WHERE baz = %s", [self.baz])
-    row = cursor.fetchone()
-
-    return row
+    def create_postgresql(self, name):
+        raise NotImplementedError
     
+    def create_mysql(self, name):
+        raise NotImplementedError
     
-#        connection = DatabaseWrapper({
-#            'DATABASE_HOST': settings.DATABASE_HOST,
-#            'DATABASE_NAME': settings.DATABASE_NAME + '_test',
-#            'DATABASE_OPTIONS': settings.DATABASE_OPTIONS,
-#            'DATABASE_PASSWORD': settings.DATABASE_PASSWORD,
-#            'DATABASE_PORT': settings.DATABASE_PORT,
-#            'DATABASE_USER': settings.DATABASE_USER,
-#            'TIME_ZONE': settings.TIME_ZONE,
-#        })
-#        connection.creation.create_test_db(verbosity, autoclobber=not interactive)
-        
-        
-        
-        
-        
-        
-        
-#from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper
-#from django.db import connection
-
-#class FlexDatabaseCreation(DatabaseCreation):
-#    def create_test_db(self, verbosity=1, autoclobber=False):
-#        ...
-#        self.connection.settings_dict["DATABASE_NAME"] = test_database_name
-#        connection.settings_dict["DATABASE_NAME"] = test_database_name # New code.
-#        ...
-#        self.connection.settings_dict["DATABASE_SUPPORTS_TRANSACTIONS"] = can_rollback
-#        connection.settings_dict["DATABASE_SUPPORTS_TRANSACTIONS"] = can_rollback # New code.
-#        
-#class FlexDatabaseWrapper(DatabaseWrapper):
-#    def __init__(self, *args, **kwargs):
-#        super(FlexDatabaseWrapper, self).__init__(*args, **kwargs)
-#        self.creation = FlexDatabaseCreation(self)
+    def create_oracle(self, name):
+        raise NotImplementedError
