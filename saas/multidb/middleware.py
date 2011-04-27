d265c2a5aa971749055bfb692b4b2d901d1213ec
Allan Lei <allanlei@helveticode.com>
2011-04-27 15:34:56 -0700

    Add Database loading middleware

--- saas/multidb/middleware.py
+++ saas/multidb/middleware.py
@@ -1,13 +1,16 @@
 from django.utils.functional import curry
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
 
 from saas.multidb.signals import db_route_read, db_route_write
+from saas.multidb.models import Database
 
 def request_router_info(sender, request=None, **kwargs):
     if request:
         return request.REQUEST.get('domain', 'default')
 
 
-class ModelRoutingMiddleware(object):
+class ModelRoutingMiddleware(object):        
     def get_signal_function(self, **kwargs):
         return curry(request_router_info, **kwargs)
     
@@ -20,3 +23,9 @@ class ModelRoutingMiddleware(object):
         db_route_read.disconnect(weak=False, dispatch_uid=request)
         db_route_write.disconnect(weak=False, dispatch_uid=request)
         return response
+
+
+class DatabaseLoadMiddleware(object):
+    def __init__(self):
+        Database.objects.all().load()
+        raise MiddlewareNotUsed
