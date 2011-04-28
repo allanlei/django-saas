5197535e88ca622e56df5b4d4bf774e5e1e43d57
Allan Lei <allanlei@helveticode.com>
2011-04-27 17:22:24 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,61 +1,15 @@
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-import os
 import sys
+import os
 
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != '':
-    os.chdir(root_dir)
-django_dir = 'saas'
-
-for dirpath, dirnames, filenames in os.walk(django_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from distutils.core import setup
 
 setup(
-    name = 'saas',
-    version = '0.1',
-    url = 'https://github.com/allanlei/django-saas',
-    author = 'Allan Lei',
-    author_email = 'allanlei@helveticode.com',
+    name='django-saas',
+    version='0.1',
     description = 'SaaS functionality for Django',
-    download_url = 'git://github.com/allanlei/django-saas.git',
+    url = 'https://github.com/allanlei/django-saas',
+    author='Allan Lei',
+    author_email='allanlei@helveticode.com',
+    download_url='https://github.com/allanlei/django-saas/tarball/master',
     license = 'New BSD',
-    packages = packages,
-    cmdclass = {
-        'install_data': install_data
-    },
-    data_files = data_files,
-    install_requires = [
-    ],
 )
