4540ae57c6ad57020f24ba8e1f6c91d90f2dd7ca
Allan Lei <allanlei@helveticode.com>
2011-06-02 12:49:16 -0700

    cleanup setup

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
-import sys
-import os
-
 from distutils.core import setup
+from setuptools import find_packages
+import os
+import sys
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def find_packages_in(where, **kwargs):
+    return [where] + ['%s.%s' % (where, package) for package in find_packages(where=where, **kwargs)]
 
 setup(
-    name='django-saas',
-    version='0.1',
-    description = 'SaaS functionality for Django',
-    url = 'https://github.com/allanlei/django-saas',
-    author='Allan Lei',
-    author_email='allanlei@helveticode.com',
-    download_url='https://github.com/allanlei/django-saas/tarball/master',
+    name = 'django-saas',
+    version = '0.1',
+    author = 'Allan Lei',
+    author_email = 'allanlei@helveticode.com',
+    description = ('SaaS for Django'),
     license = 'New BSD',
+    keywords = 'saas multidb multischema django',
+    url = 'https://github.com/allanlei/django-saas',
+    packages=find_packages_in('saas'),
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
